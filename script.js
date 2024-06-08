c90b9ebcb337ea8c80511cbce299f1f786eea6ca
Ashley <ashleykountlea@gmail.com>
2024-06-08 20:38:09 +0100

    CHALLENGE1_TUTORIAL

--- script.js
+++ script.js
@@ -1,47 +1,92 @@
-let contain1 = document.querySelector('.container1') ;
-let contain2 = document.querySelector('.container2') ;
-let contain3 = document.querySelector('.container3') ;
-let contain4 = document.querySelector('.container4') ;
-let contain5 = document.querySelector('.container5') ;
+const panels = document.querySelectorAll('.panel');
+//console.log(panels) ;
+panels.forEach(panel => {
+    panel.addEventListener('click', () =>{
+        removeActiveClasses();
+        panel.classList.add('active')
+    }) ;
+})
 
-function click1(){
-    //alert("Vous m'avez cliqué !");
-    contain1.style.width = '70vw'; 
-    contain2.style.width = '5vw'; 
-    contain3.style.width = '5vw';
-    contain4.style.width = '5vw'; 
-    contain5.style.width = '5vw'; 
-}
-function click2(){
-    //alert("Vous m'avez cliqué !");
-    contain1.style.width = '5vw'; 
-    contain2.style.width = '70vw'; 
-    contain3.style.width = '5vw';
-    contain4.style.width = '5vw'; 
-    contain5.style.width = '5vw'; 
-}
-function click3(){
-    //alert("Vous m'avez cliqué !");
-    contain1.style.width = '5vw'; 
-    contain2.style.width = '5vw'; 
-    contain3.style.width = '70vw';
-    contain4.style.width = '5vw'; 
-    contain5.style.width = '5vw'; 
+function removeActiveClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    });
 }
 
-function click4(){
-    //alert("Vous m'avez cliqué !");
-    contain1.style.width = '5vw'; 
-    contain2.style.width = '5vw'; 
-    contain3.style.width = '5vw';
-    contain4.style.width = '70vw'; 
-    contain5.style.width = '5vw';  
-}
-function click5(){
-    //alert("Vous m'avez cliqué !");
-    contain1.style.width = '5vw'; 
-    contain2.style.width = '5vw'; 
-    contain3.style.width = '5vw';
-    contain4.style.width = '5vw'; 
-    contain5.style.width = '70vw'; 
-}
+
+
+                            // RELEASE_V2.1
+
+// let contain1 = document.querySelector('.container1') ;
+// let contain2 = document.querySelector('.container2') ;
+// let contain3 = document.querySelector('.container3') ;
+// let contain4 = document.querySelector('.container4') ;
+// let contain5 = document.querySelector('.container5') ;
+// let h3 = document.querySelectorAll('h3');
+
+// function click1(){
+//     //alert("Vous m'avez cliqué !");
+//     contain1.style.width = '70vw'; 
+//     contain2.style.width = '5vw'; 
+//     contain3.style.width = '5vw';
+//     contain4.style.width = '5vw'; 
+//     contain5.style.width = '5vw'; 
+//     h3[0].style.color = "rgba(250, 250, 250, 1)"; 
+//     h3[1].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[2].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[3].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[4].style.color = "rgba(250, 250, 250, 0)"; 
+// }
+// function click2(){
+//     //alert("Vous m'avez cliqué !");
+//     contain1.style.width = '5vw'; 
+//     contain2.style.width = '70vw';
+//     contain3.style.width = '5vw';
+//     contain4.style.width = '5vw'; 
+//     contain5.style.width = '5vw'; 
+//     h3[0].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[1].style.color = "rgba(250, 250, 250, 1)"; 
+//     h3[2].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[3].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[4].style.color = "rgba(250, 250, 250, 0)";  
+// }
+// function click3(){
+//     //alert("Vous m'avez cliqué !");
+//     contain1.style.width = '5vw'; 
+//     contain2.style.width = '5vw'; 
+//     contain3.style.width = '70vw';
+//     contain4.style.width = '5vw'; 
+//     contain5.style.width = '5vw'; 
+//     h3[0].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[1].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[2].style.color = "rgba(250, 250, 250, 1)"; 
+//     h3[3].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[4].style.color = "rgba(250, 250, 250, 0)"; 
+// }
+
+// function click4(){
+//     //alert("Vous m'avez cliqué !");
+//     contain1.style.width = '5vw'; 
+//     contain2.style.width = '5vw'; 
+//     contain3.style.width = '5vw';
+//     contain4.style.width = '70vw'; 
+//     contain5.style.width = '5vw'; 
+//     h3[0].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[1].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[2].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[3].style.color = "rgba(250, 250, 250, 1)"; 
+//     h3[4].style.color = "rgba(250, 250, 250, 0)"; 
+// }
+// function click5(){
+//     //alert("Vous m'avez cliqué !");
+//     contain1.style.width = '5vw'; 
+//     contain2.style.width = '5vw'; 
+//     contain3.style.width = '5vw';
+//     contain4.style.width = '5vw'; 
+//     contain5.style.width = '70vw'; 
+//     h3[0].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[1].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[2].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[3].style.color = "rgba(250, 250, 250, 0)"; 
+//     h3[4].style.color = "rgba(250, 250, 250, 1)"; 
+// }
