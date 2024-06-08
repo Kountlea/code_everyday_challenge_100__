6badfd4aea7975458f1a6f01903dc096a89a3648
Ashley <ashleykountlea@gmail.com>
2024-06-08 12:16:24 +0100

    RELEASE_V1

---
+++ script.js
@@ -0,0 +1,47 @@
+let contain1 = document.querySelector('.container1') ;
+let contain2 = document.querySelector('.container2') ;
+let contain3 = document.querySelector('.container3') ;
+let contain4 = document.querySelector('.container4') ;
+let contain5 = document.querySelector('.container5') ;
+
+function click1(){
+    //alert("Vous m'avez cliqué !");
+    contain1.style.width = '35vw'; 
+    contain2.style.width = '5vw'; 
+    contain3.style.width = '5vw';
+    contain4.style.width = '5vw'; 
+    contain5.style.width = '5vw'; 
+}
+function click2(){
+    //alert("Vous m'avez cliqué !");
+    contain1.style.width = '5vw'; 
+    contain2.style.width = '35vw'; 
+    contain3.style.width = '5vw';
+    contain4.style.width = '5vw'; 
+    contain5.style.width = '5vw'; 
+}
+function click3(){
+    //alert("Vous m'avez cliqué !");
+    contain1.style.width = '5vw'; 
+    contain2.style.width = '5vw'; 
+    contain3.style.width = '35vw';
+    contain4.style.width = '5vw'; 
+    contain5.style.width = '5vw'; 
+}
+
+function click4(){
+    //alert("Vous m'avez cliqué !");
+    contain1.style.width = '5vw'; 
+    contain2.style.width = '5vw'; 
+    contain3.style.width = '5vw';
+    contain4.style.width = '35vw'; 
+    contain5.style.width = '5vw';  
+}
+function click5(){
+    //alert("Vous m'avez cliqué !");
+    contain1.style.width = '5vw'; 
+    contain2.style.width = '5vw'; 
+    contain3.style.width = '5vw';
+    contain4.style.width = '5vw'; 
+    contain5.style.width = '35vw'; 
+}
